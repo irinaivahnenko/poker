83d0befbc6a0006197c1ed0f4423a1fd3a72d0ab
Irina Ivahnenko <irina.ivahnenko@gmail.com>
2014-12-08 22:07:54 +0200

    Taiendasin UI, GameInterface ja CardImagePanel. Lisatud kaardipildid ja Teksti kuvamine.

--- src/ee/irina/poker/CardImagePanel.java
+++ src/ee/irina/poker/CardImagePanel.java
@@ -5,15 +5,20 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Irina Ivahnenko on 8.12.14.
  */
 public class CardImagePanel extends JPanel {
-    private BufferedImage image;
-    public CardImagePanel(){
+    public CardImagePanel(List<Card> cards){
         try{
-            image = ImageIO.read(new File("card_pictures/pilt.jpg"));
+            for (int i = 0; i<cards.size();i++){
+                String fileName = cards.get(i).getSuit().name().substring(0, 1).toLowerCase() + String.valueOf(cards.get(i).getValue());
+                BufferedImage image = ImageIO.read(new File("card_pictures/" + fileName + ".png"));
+                JLabel picLabel = new JLabel(new ImageIcon(image));
+                add(picLabel);
+            }
 
         } catch(IOException exeption){
             System.out.print("Pilti ei leitud!");
--- src/ee/irina/poker/GameInterface.java
+++ src/ee/irina/poker/GameInterface.java
@@ -2,11 +2,13 @@ package ee.irina.poker;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author Irina Ivahnenko
@@ -39,9 +41,40 @@ public class GameInterface extends JPanel implements ActionListener {
         //Akna sisu määramine
         GameInterface newContentPane = new GameInterface();
         //sisu peab olema läbipaistev
+
+        // Siin tuleb teha mangija objekt
+        GamePlayer player = new GamePlayer();
+
+        // Siis tuleb teha kaardipakk.
+        Deck deck = new Deck();
+
+        // Segame kaardipaki
+        deck.shuffle();
+
+        // Jagame mängijale esimese käe
+        player.setCards(deck.getHand());
+
+        // Siin võiks printida, mis käsi inimesel on.
+        //player.printCards();
+
+        // Hindame kae kombinatsiooni
+        RankEvaluator evaluator = new RankEvaluator(player);
+        int rank = evaluator.evaluate();
+
+        // Siin trükime välja käe kombinatsiooni.
+        System.out.println(RankEvaluator.ranks[rank]);
+
+        //Loon tekstipaneeli selleks, et kuvada kaardi kae kombinatsiooni ekraanil
+        JLabel label = new JLabel(RankEvaluator.ranks[rank]);
+        newContentPane.add(label);
+
+        //Loon kaartide näitamise ekraanil
+        CardImagePanel cardImagePanel = new CardImagePanel(player.getCards());
+        newContentPane.add(cardImagePanel);
+
+        //Määran aknale sisu muutujaga newContentPane (private)
         newContentPane.setOpaque(true);
         frame.setContentPane(newContentPane);
-
         //Teeb akna nähtavaks
         frame.pack();
         frame.setVisible(true);
